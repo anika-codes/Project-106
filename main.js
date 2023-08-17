6bc6192e8216aa31578b8d140da8df00fa7f6fd4
anika-codes <82987182+anika-codes@users.noreply.github.com>
2023-08-17 10:32:08 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -13,5 +13,41 @@ function modelReady(){
 }
 
 function got_results(error, results){
-    console.log("we got results")
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results)
+
+        r = Math.floor(Math.random()*255)+1;
+        g = Math.floor(Math.random()*255)+1;
+        b = Math.floor(Math.random()*255)+1;
+
+        sound_name =  results[0].label;
+        document.getElementById("sound").innerHTML = sound_name;
+        accuracy = (results[0].confidence*100).toFixed(2);
+        document.getElementById("accuracy").innerHTML = accuracy + "%";
+
+        document.getElementById("sound").style.color = "rgb(" + r + "," + g + "," + b + ")";
+        document.getElementById("accuracy").style.color = "rgb(" + r + "," + g + "," + b + ")";
+
+        picture = document.getElementById("picture");
+
+        if(sound_name == "Background Noise"){
+            picture.src = "colorear-removebg-preview.png";
+        }
+        else if(sound_name == "cat meowing"){
+            picture.src = "cat.webp";
+        }
+        else if(sound_name == "Cow Mooing"){
+            picture.src = "cow.webp";
+        }
+        else if(sound_name == "Pig Noises"){
+            picture.src = "pig.jpg";
+        }
+        else if(sound_name == "Dog Barking"){
+            picture.src = "dog.png";
+        }
+
+    }
 }
